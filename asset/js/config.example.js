786a62db38ed8830729d8f5e0e79f83334f0a66f
Kyle Ladd <kladd6@gmail.com>
2016-01-27 00:34:02 -0500

    API Config, Namspacing JS

---
+++ asset/js/config.example.js
@@ -0,0 +1,7 @@
+var mscSchedulizer = mscSchedulizer === undefined ? {} : mscSchedulizer;
+$.extend(mscSchedulizer, {
+    api_host:"http://schedulizer-api.kyleladd.us/v1",
+    course_selections:"#course_selections",
+    departments:"#departments",
+    department_class_list:"#dept_class_list"
+});
--- asset/js/schedulizer.js
+++ asset/js/schedulizer.js
@@ -1,118 +1,110 @@
- $(function(){ 
-        var api_host = "http://schedulizer-api.kyleladd.us/v1";
-        //Storage
-        var classes_selected = JSON.parse(localStorage.getItem('classes_selected')) || [];
-        var favorite_schedules = JSON.parse(localStorage.getItem('favorite_schedules')) || [];
-        loadSelections();
-
-        function listContainsDictionaryIndex(list,keyvaluelist){
-            try{
-                var numcriterion = 0;
+var mscSchedulizer = mscSchedulizer === undefined ? {} : mscSchedulizer;
+$.extend(mscSchedulizer, {
+    classes_selected: JSON.parse(localStorage.getItem('classes_selected')) || [],
+    favorite_schedules: JSON.parse(localStorage.getItem('favorite_schedules')) || [],
+    listContainsDictionaryIndex: function(list,keyvaluelist){
+        try{
+            var numcriterion = 0;
+            for (var key in keyvaluelist) {
+                numcriterion++;
+            }
+            for(var i = 0; i < list.length; i++){
+                var counter = 0;
                 for (var key in keyvaluelist) {
-                    numcriterion++;
-                }
-                for(var i = 0; i < list.length; i++){
-                    var counter = 0;
-                    for (var key in keyvaluelist) {
-                        if(list[i][key] == keyvaluelist[key]){
-                            counter++;
-                            if(counter == numcriterion){
-                                return i;
-                            }
+                    if(list[i][key] == keyvaluelist[key]){
+                        counter++;
+                        if(counter == numcriterion){
+                            return i;
                         }
                     }
                 }
-                return -1;
             }
-            catch(err){
-                return -1;
-            }
+            return -1;
         }
-
-        function loadSelections() {
-            var output = "";
-            $.each(classes_selected, function(i, course){
-                output += "<a href=\"#\" data-value='"+JSON.stringify(course)+"' class=\"a_selection\">"+course.departments.abbreviation+" " + course.courseNumber + " <i class=\"fa fa-times\"></i></a>";
-            });
-            $("#course_selections").html(output);
+        catch(err){
+            return -1;
         }
-
-        //Get From Endpoint
-        $.getJSON(api_host + "/departments/", function(results){
-            $.each(results, function(i, department){
-                $("#departments").append("<li><a class='a_department' data-value='"+department.id+"'>"+department.abbreviation+"</a></li>");
-            });
-        })
-        .fail(function() {
-            $("#departments").html("<li>Unable to load departments.</li>");
-        })
-        .always(function() {
-            $("#departments").removeClass("loader-large");
-            $("#departments").removeClass("loader");
+    },
+    loadSelections: function(){
+        var output = "";
+        $.each(mscSchedulizer.classes_selected, function(i, course){
+            output += "<a href=\"#\" data-value='"+JSON.stringify(course)+"' class=\"a_selection\">"+course.departments.abbreviation+" " + course.courseNumber + " <i class=\"fa fa-times\"></i></a>";
         });
-
-        //Interactivity
-         $.getJSON(api_host + "/courses/?department_id=1", function(results){
+        $(mscSchedulizer.course_selections).html(output);
+    },
+    getDepartmentCourses: function(department){
+        department = typeof department !== 'undefined' ?  department : 1;
+        $.getJSON(mscSchedulizer.api_host + "/courses/?department_id=" + department, function(results){
             //remove this later
-            $("#dept_class_list").html("");
+            var output = "";
             $.each(results, function(i, course){
                 //Change to just one html output set
-                $("#dept_class_list").append("<li><a class='a_course' data-value='"+JSON.stringify(course)+"'>"+course.departments.abbreviation+" " + course.courseNumber +" - " + course.name + "</a></li>");
+                output += "<li><a class='a_course' data-value='"+JSON.stringify(course)+"'>"+course.departments.abbreviation+" " + course.courseNumber +" - " + course.name + "</a></li>";
             });
+            $(mscSchedulizer.department_class_list).html(output);
         })
         .fail(function() {
-            $("#dept_class_list").html("<li>Unable to load courses.</li>");
+            $(mscSchedulizer.department_class_list).html("<li>Unable to load courses.</li>");
         })
         .always(function() {
-            $("#dept_class_list").removeClass("loader-large");
-            $("#dept_class_list").removeClass("loader");
+            $(mscSchedulizer.department_class_list).removeClass("loader-large");
+            $(mscSchedulizer.department_class_list).removeClass("loader");
         });
-        // Department selected - View Department Courses
-        $("#departments").on("click", "a.a_department", function (event) {
-            event.preventDefault();
-            $("#dept_class_list").addClass("loader");
-            //Get From Endpoint - Department Courses
-            $.getJSON(api_host + "/courses/?department_id="+this.getAttribute('data-value'), function(results){
-                //remove this later
-                $("#dept_class_list").html("");
-                $.each(results, function(i, course){
-                    //Change to just one html output set
-                    $("#dept_class_list").append("<li><a class='a_course' data-value='"+JSON.stringify(course)+"'>"+course.departments.abbreviation+" " + course.courseNumber +" - " + course.name + "</a></li>");
-                });
-            })
-            .fail(function() {
-                $("#dept_class_list").html("<li>Unable to load courses.</li>");
-            })
-            .always(function() {
-                $("#dept_class_list").removeClass("loader-large");
-                $("#dept_class_list").removeClass("loader");
+    },
+    getDepartments:function(){
+        $.getJSON(mscSchedulizer.api_host + "/departments/", function(results){
+            var output = "";
+            $.each(results, function(i, department){
+                output += "<li><a class='a_department' data-value='"+department.id+"'>"+department.abbreviation+"</a></li>";
             });
+            $(mscSchedulizer.departments).html(output);
+        })
+        .fail(function() {
+            $(mscSchedulizer.departments).html("<li>Unable to load departments.</li>");
+        })
+        .always(function() {
+            $(mscSchedulizer.departments).removeClass("loader-large");
+            $(mscSchedulizer.departments).removeClass("loader");
         });
-        // Course selected - Add Course
-        $("#dept_class_list").on("click", "a.a_course", function (event) {
-            event.preventDefault();
-            //Add Course
-            var course = JSON.parse(this.getAttribute('data-value'));
-            if (listContainsDictionaryIndex(classes_selected,{id:course.id}) === -1) {
-                classes_selected.push(course);
-                localStorage.setItem('classes_selected', JSON.stringify(classes_selected));
-                //reload selections area
-                loadSelections();
-            }
-        });
+    }
+});
 
-         // Remove Course Selections
-        $("#course_selections").on("click", "a.a_selection", function (event) {
-            event.preventDefault();
-            // //Remove Course
-            var course = JSON.parse(this.getAttribute('data-value'));
-            var index = listContainsDictionaryIndex(classes_selected,{id:course.id});
-            if (index != -1) {
-                classes_selected.splice(index,1);
-                localStorage.setItem('classes_selected', JSON.stringify(classes_selected));
-                //reload selections area
-                loadSelections();
-            }
-        });
+$(function(){ 
+    mscSchedulizer.loadSelections();
+    mscSchedulizer.getDepartments();
+    mscSchedulizer.getDepartmentCourses();
 
+    // Department selected - View Department Courses
+    $(mscSchedulizer.departments).on("click", "a.a_department", function (event) {
+        event.preventDefault();
+        $(mscSchedulizer.department_class_list).addClass("loader");
+        //Get From Endpoint - Department Courses
+        mscSchedulizer.getDepartmentCourses(this.getAttribute('data-value'));
     });
+    // Course selected - Add Course
+    $(mscSchedulizer.department_class_list).on("click", "a.a_course", function (event) {
+        event.preventDefault();
+        //Add Course
+        var course = JSON.parse(this.getAttribute('data-value'));
+        if (mscSchedulizer.listContainsDictionaryIndex(mscSchedulizer.classes_selected,{id:course.id}) === -1) {
+            mscSchedulizer.classes_selected.push(course);
+            localStorage.setItem('classes_selected', JSON.stringify(mscSchedulizer.classes_selected));
+            //reload selections area
+            mscSchedulizer.loadSelections();
+        }
+    });
+
+     // Remove Course Selections
+    $(mscSchedulizer.course_selections).on("click", "a.a_selection", function (event) {
+        event.preventDefault();
+        // //Remove Course
+        var course = JSON.parse(this.getAttribute('data-value'));
+        var index = mscSchedulizer.listContainsDictionaryIndex(mscSchedulizer.classes_selected,{id:course.id});
+        if (index != -1) {
+            mscSchedulizer.classes_selected.splice(index,1);
+            localStorage.setItem('classes_selected', JSON.stringify(mscSchedulizer.classes_selected));
+            //reload selections area
+            mscSchedulizer.loadSelections();
+        }
+    });
+});
